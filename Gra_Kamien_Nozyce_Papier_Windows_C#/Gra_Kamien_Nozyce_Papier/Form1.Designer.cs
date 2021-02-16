891e7ec9038e111f8719e5aa624a556d74afe7e4
Paweł <30297982+esiu06@users.noreply.github.com>
2021-02-16 12:16:58 +0100

    Add files via upload
    Game Stone Paper Scissors; windows version of C #

---
+++ Gra_Kamien_Nozyce_Papier_Windows_C#/Gra_Kamien_Nozyce_Papier/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class GRA
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GRA));
+            this.label2 = new System.Windows.Forms.Label();
+            this.bKamien = new System.Windows.Forms.Button();
+            this.bNozyce = new System.Windows.Forms.Button();
+            this.bPapier = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(47, 66);
+            this.label2.MinimumSize = new System.Drawing.Size(530, 0);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(8);
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(530, 54);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "GRA KAMIEŃ - NOŻYCE - PAPIER";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // bKamien
+            // 
+            this.bKamien.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.bKamien.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bKamien.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bKamien.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bKamien.Location = new System.Drawing.Point(76, 168);
+            this.bKamien.Name = "bKamien";
+            this.bKamien.Size = new System.Drawing.Size(121, 51);
+            this.bKamien.TabIndex = 1;
+            this.bKamien.Text = "KAMIEŃ";
+            this.bKamien.UseVisualStyleBackColor = false;
+            this.bKamien.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // bNozyce
+            // 
+            this.bNozyce.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.bNozyce.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bNozyce.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bNozyce.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bNozyce.Location = new System.Drawing.Point(259, 168);
+            this.bNozyce.Name = "bNozyce";
+            this.bNozyce.Size = new System.Drawing.Size(121, 51);
+            this.bNozyce.TabIndex = 2;
+            this.bNozyce.Text = "NOŻYCE";
+            this.bNozyce.UseVisualStyleBackColor = false;
+            this.bNozyce.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // bPapier
+            // 
+            this.bPapier.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.bPapier.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bPapier.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bPapier.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bPapier.Location = new System.Drawing.Point(435, 168);
+            this.bPapier.Name = "bPapier";
+            this.bPapier.Size = new System.Drawing.Size(121, 51);
+            this.bPapier.TabIndex = 3;
+            this.bPapier.Text = "PAPIER";
+            this.bPapier.UseVisualStyleBackColor = false;
+            this.bPapier.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(82, 274);
+            this.label3.MinimumSize = new System.Drawing.Size(175, 50);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(8);
+            this.label3.Size = new System.Drawing.Size(175, 50);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Twój wybór";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(82, 383);
+            this.label4.MinimumSize = new System.Drawing.Size(175, 50);
+            this.label4.Name = "label4";
+            this.label4.Padding = new System.Windows.Forms.Padding(8);
+            this.label4.Size = new System.Drawing.Size(175, 50);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Wybór komputera";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.ForeColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(336, 399);
+            this.button4.Name = "button4";
+            this.button4.Padding = new System.Windows.Forms.Padding(8);
+            this.button4.Size = new System.Drawing.Size(200, 54);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "SPRAWDZAM";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(363, 274);
+            this.label5.MinimumSize = new System.Drawing.Size(150, 70);
+            this.label5.Name = "label5";
+            this.label5.Padding = new System.Windows.Forms.Padding(8);
+            this.label5.Size = new System.Drawing.Size(150, 70);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Wynik";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.Location = new System.Drawing.Point(47, 525);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(141, 51);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Statystyka";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button6.Location = new System.Drawing.Point(241, 525);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(141, 51);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Zapisz";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button7.Location = new System.Drawing.Point(435, 525);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(141, 51);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Wyjście";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(83, 324);
+            this.label6.MinimumSize = new System.Drawing.Size(175, 20);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(175, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Twój wybór";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(82, 433);
+            this.label7.MinimumSize = new System.Drawing.Size(175, 20);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(175, 20);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Wybór komputera";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(455, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 39);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Zasady gry";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(47, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 39);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Ustawienia";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // GRA
+            // 
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(625, 599);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.bPapier);
+            this.Controls.Add(this.bNozyce);
+            this.Controls.Add(this.bKamien);
+            this.Controls.Add(this.label2);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Name = "GRA";
+            this.Text = "GRA KAMIEŃ-NOŻYCE-PAPIER";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button bKamien;
+        private System.Windows.Forms.Button bNozyce;
+        private System.Windows.Forms.Button bPapier;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Gra_Kamien_Nozyce_Papier_Windows_C#/Gra_Kamien_Nozyce_Papier/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class GRA : Form
+    {
+
+        public GRA()
+        {
+            InitializeComponent();
+        }
+       
+
+        public static string[] wyborGracza = { "Nożyce", "Kamień", "Papier" };
+
+        public static string[] wyborKomputera = { "Nożyce", "Kamień", "Papier" };
+
+        public static string ruchGry = wyborKomputera[0];
+
+        public static string ruchGracza = wyborGracza[0];
+
+        public static int pozycjaMenu = 0;
+
+        public static int remisy = 0;
+
+        public static int wygrane = 0;
+
+        public static int przegrane = 0;
+
+        public static int iloscGier = 0;
+
+        public static string nick;
+
+        public static string wynik;
+        
+
+        public static void StanGry()
+
+        {
+
+            if (ruchGracza.Equals(wyborKomputera[0]) && ruchGry.Equals(wyborGracza[0]))
+            {
+                wynik = "Remis";
+                remisy++;
+                iloscGier++;
+
+            }
+            if (ruchGracza.Equals(wyborKomputera[0]) && ruchGry.Equals(wyborGracza[1]))
+            {
+                wynik = "Przegrałeś!";
+                przegrane++;
+                iloscGier++;
+            }
+            if (ruchGracza.Equals(wyborKomputera[0]) && ruchGry.Equals(wyborGracza[2]))
+            {
+                wynik = "Wygrałeś!";
+                wygrane++;
+                iloscGier++;
+            }
+            if (ruchGracza.Equals(wyborKomputera[1]) && ruchGry.Equals(wyborGracza[1]))
+            {
+                wynik = "Remis!";
+                remisy++;
+                iloscGier++;
+            }
+            if (ruchGracza.Equals(wyborKomputera[1]) && ruchGry.Equals(wyborGracza[0]))
+            {
+                wynik = "Wygrałeś!";
+                wygrane++;
+                iloscGier++;
+            }
+            if (ruchGracza.Equals(wyborKomputera[1]) && ruchGry.Equals(wyborGracza[2]))
+            {
+                wynik = "Przegrałeś!";
+                przegrane++;
+                iloscGier++;
+            }
+            if (ruchGracza.Equals(wyborKomputera[2]) && ruchGry.Equals(wyborGracza[2]))
+            {
+                wynik = "Remis!";
+                remisy++;
+                iloscGier++;
+            }
+            if (ruchGracza.Equals(wyborKomputera[2]) && ruchGry.Equals(wyborGracza[1]))
+            {
+                wynik = "Wygrałeś!";
+                wygrane++;
+                iloscGier++;
+            }
+            if (ruchGracza.Equals(wyborKomputera[2]) && ruchGry.Equals(wyborGracza[0]))
+            {
+                wynik = "Przegrałeś!";
+                przegrane++;
+                iloscGier++;
+            }
+        }
+        private void Zapis()
+        {
+            string[] statystyka = { "Ilość gier: " + iloscGier,
+                "Wygranych: " + wygrane, "Przegranych: " + przegrane,
+                "Remisy: " + remisy };
+
+            string sciezkaPliku = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            string nazwaPliku = "Statystyki.txt";
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(sciezkaPliku, nazwaPliku)))
+            {
+                foreach (string linia in statystyka)
+                    outputFile.WriteLine(linia);
+                MessageBox.Show("Statystyka została zapisana w: " + sciezkaPliku);
+            }
+
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label3.Text = wyborGracza[0];
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label3.Text = wyborGracza[2];
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = wyborGracza[1];
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            Random rand = new Random();
+
+            switch (rand.Next(3))
+            {
+                case 0:
+
+                    label4.Text  = wyborKomputera[0];
+                    
+                    break;
+                case 1:
+                    label4.Text  = wyborKomputera[1];
+                    
+                    break;
+                case 2:
+                    label4.Text  = wyborKomputera[2];
+                    
+                    break;
+
+            }
+
+            ruchGracza = label3.Text;
+            ruchGry = label4.Text;
+            StanGry();
+            label5.Text = wynik;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ilość gier: " + iloscGier + "\n\nWygranych: " + wygrane + "\n\nPrzegrane: " + przegrane + "\n\nRemisy: " + remisy);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Zapis();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Zasady gry:\n\nGra składa się z kolejnych tur. W każdej turze obydwaj gracze," +
+                "na umówiony sygnał, szybko wystawiają przed siebie dłoń, pokazującą symbol papieru," +
+                "kamienia lub nożyc. Gracz, który pokazał silniejszy symbol, otrzymuje jeden punkt." +
+                "W przypadku pokazania dwóch takich samych symboli następuje remis – punktu brak.\n\n" +
+                "Oto hierarchia symboli:\n\n- nożyce są silniejsze od papieru, ponieważ go tną,\n" +
+                "- kamień jest silniejszy od nożyc, ponieważ je tępi,\n" +
+                "- papier jest silniejszy od kamienia, ponieważ go owija.\n\n" +
+                "Gracz, który pierwszy uzyska umówioną wcześniej ilość punktów, wygrywa partię.");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
